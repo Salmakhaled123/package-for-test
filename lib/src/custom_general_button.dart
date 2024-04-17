e53c1616ddd5ddc38acb897a31ba8941201b0d7b
Salmakhaled123 <toty5alouda@gmail.com>
2024-04-17 22:13:26 +0200

    first commit

---
+++ lib/src/custom_general_button.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class CustomGeneralButton extends StatelessWidget {
+  const CustomGeneralButton({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      color: Colors.green,
+      width: 150,
+      height: 150,
+    );
+  }
+}
